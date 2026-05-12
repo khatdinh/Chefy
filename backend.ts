3ec13785b2f97adaaed06f68e3008934a486bba7
khatdinh <khatdinh@gmail.com>
2026-05-12 17:16:25 -0400

    Add files via upload

---
+++ backend.ts
@@ -0,0 +1,43 @@
+import { defineBackend } from "@aws-amplify/backend";
+import { Stack } from "aws-cdk-lib";
+import { CorsHttpMethod, HttpApi, HttpMethod } from "aws-cdk-lib/aws-apigatewayv2";
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
+import { recipeFunction } from "./functions/recipe/resource";
+
+const backend = defineBackend({
+  recipeFunction,
+});
+
+const apiStack = backend.createStack("chefcraft-api");
+const recipeIntegration = new HttpLambdaIntegration(
+  "RecipeIntegration",
+  backend.recipeFunction.resources.lambda,
+);
+
+const httpApi = new HttpApi(apiStack, "ChefCraftHttpApi", {
+  apiName: "chefcraft-api",
+  createDefaultStage: true,
+  corsPreflight: {
+    allowHeaders: ["content-type"],
+    allowMethods: [CorsHttpMethod.POST, CorsHttpMethod.OPTIONS],
+    allowOrigins: ["*"],
+  },
+});
+
+httpApi.addRoutes({
+  path: "/recipe",
+  methods: [HttpMethod.POST],
+  integration: recipeIntegration,
+});
+
+backend.addOutput({
+  custom: {
+    API: {
+      [httpApi.httpApiName!]: {
+        endpoint: httpApi.url!,
+        region: Stack.of(httpApi).region,
+        apiName: httpApi.httpApiName!,
+      },
+    },
+  },
+});
